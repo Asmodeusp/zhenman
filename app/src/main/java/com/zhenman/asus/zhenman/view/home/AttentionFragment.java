cb1f10f83aa7ca904cd7d812f94e464a41587145
Prisoner <762063243@qq.com>
2018-08-21 14:43:10 +0800

    修改主页

---
+++ app/src/main/java/com/zhenman/asus/zhenman/view/home/AttentionFragment.java
@@ -0,0 +1,30 @@
+package com.zhenman.asus.zhenman.view.home;
+
+
+import com.zhenman.asus.zhenman.R;
+import com.zhenman.asus.zhenman.base.BaseFragment;
+
+public class AttentionFragment extends BaseFragment {
+
+
+    public AttentionFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.fragment_attention;
+    }
+
+    @Override
+    protected void init() {
+
+    }
+
+    @Override
+    protected void loadDate() {
+
+    }
+
+}
