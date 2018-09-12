892018a6d805bdbd60ece95bc1a1d5681674f73b
YSunnyU <2633492025@qq.com>
2018-09-12 11:27:18 +0800

    修改个人资料

---
+++ app/src/main/java/com/zhenman/asus/zhenman/utils/photo/BitmapUtils.java
@@ -0,0 +1,94 @@
+package com.zhenman.asus.zhenman.utils.photo;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Environment;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class BitmapUtils {
+    private static Bitmap compressImage(Bitmap image) {
+
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        image.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+        int options = 100;
+        while (baos.toByteArray().length / 1024 > 100) {
+            baos.reset();
+            image.compress(Bitmap.CompressFormat.JPEG, options, baos);
+            options -= 10;
+        }
+        ByteArrayInputStream isBm = new ByteArrayInputStream(baos.toByteArray());
+        Bitmap bitmap = BitmapFactory.decodeStream(isBm, null, null);
+        return bitmap;
+    }
+
+    private static String sacveMyBitmap(String fileName, Bitmap bitmap) {
+        String baseDir = Environment.getExternalStorageDirectory().getAbsolutePath() + "/zhenman/";
+        String filePath = baseDir + fileName;
+        File file = new File(baseDir);
+        //如果不存在就创建文件夹
+        if (!file.exists()) {
+            file.mkdir();
+        }
+        File f = new File(filePath);
+        try {
+            f.createNewFile();
+            FileOutputStream fileOutputStream = null;
+            fileOutputStream = new FileOutputStream(f);
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, fileOutputStream);
+            fileOutputStream.flush();
+            fileOutputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return filePath;
+    }
+
+    /**
+     * 图片按比例大小压缩方法（根据路径获取图片并压缩）
+     *
+     * @param srcPath
+     * @return
+     */
+    private static Bitmap getImage(String srcPath) {
+        BitmapFactory.Options newOpts = new BitmapFactory.Options();
+        newOpts.inJustDecodeBounds = true;
+        Bitmap bitmap = BitmapFactory.decodeFile(srcPath, newOpts);// 此时返回bm为空
+
+        newOpts.inJustDecodeBounds = false;
+        int w = newOpts.outWidth;
+        int h = newOpts.outHeight;
+        // 现在主流手机比较多是800*480分辨率，所以高和宽我们设置为
+        float hh = 800f;// 这里设置高度为800f
+        float ww = 480f;// 这里设置宽度为480f
+        // 缩放比。由于是固定比例缩放，只用高或者宽其中一个数据进行计算即可
+        int be = 1;// be=1表示不缩放
+        if (w > h && w > ww) {// 如果宽度大的话根据宽度固定大小缩放
+            be = (int) (newOpts.outWidth / ww);
+        } else if (w < h && h > hh) {// 如果高度高的话根据宽度固定大小缩放
+            be = (int) (newOpts.outHeight / hh);
+        }
+        if (be <= 0)
+            be = 1;
+        newOpts.inSampleSize = be;// 设置缩放比例
+        // 重新读入图片，注意此时已经把options.inJustDecodeBounds 设回false了
+        bitmap = BitmapFactory.decodeFile(srcPath, newOpts);
+        return compressImage(bitmap);// 压缩好比例大小后再进行质量压缩
+    }
+    /**
+     * 压缩上传路径
+     *
+     * @param path
+     * @return
+     */
+    public static String compressImageUpload(String path) {
+        String filename = path.substring(path.lastIndexOf("/") + 1);
+        Bitmap image = getImage(path);
+        return sacveMyBitmap(filename, image);
+    }
+
+}
