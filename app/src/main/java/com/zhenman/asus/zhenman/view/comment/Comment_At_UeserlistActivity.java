64334487525dfe608c5a4a90fa3a4f75224a8019
Prisoner <762063243@qq.com>
2018-10-24 15:41:07 +0800

    添加弹出@列表

---
+++ app/src/main/java/com/zhenman/asus/zhenman/view/comment/Comment_At_UeserlistActivity.java
@@ -0,0 +1,15 @@
+package com.zhenman.asus.zhenman.view.comment;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.zhenman.asus.zhenman.R;
+
+public class Comment_At_UeserlistActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_comment_at_ueserlist);
+    }
+}
